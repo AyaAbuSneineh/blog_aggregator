d63816760aa0db5abdfa7b561a9caa6214487ad5
AyaAbuSneineh <aya.abusneineh2004@gmail.com>
2026-07-23 13:43:09 +0300

    Final edit

--- src/commands.ts
+++ src/commands.ts
@@ -1,20 +1,22 @@
 import { setUser} from "./config.js";
 
-export type CommandHandler = (cmdName: string, ...args: string[]) => void;
-export type CommandsRegistry =Record<string, CommandHandler>; 
+export type CommandHandler = (cmdName: string, ...args: string[]) => void; // function type
+export type CommandEntry = [cmdName: string, handler: CommandHandler]; // tuple type
+export type CommandsRegistry =CommandEntry[];    // array of tuples
 
+// we use this function to verify username is provided and set the user
 export function handlerLogin(cmdName: string, ...args: string[]): void {
-  if (args.length<1) {
+  if (args.length < 1) {
     throw new Error("username is required");
   }
-  setUser(args[0]);
+  setUser(args[0]); // set the user in config
   console.log(`User set to ${args[0]}`);
 }
 export function registerCommand(registry: CommandsRegistry, cmdName: string, handler: CommandHandler): void { // Register a command handler
-  registry[cmdName] = handler;
+  registry.push([cmdName, handler]);
 }
 export function runCommand(registry: CommandsRegistry, cmdName: string, ...args: string[]): void { // Run a command handler
-  const handler = registry[cmdName];
+  const handler = registry.find(([name]) => name === cmdName)?.[1];
   if (!handler) {
     throw new Error(`Unknown command: ${cmdName}`);
   }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,26 @@
 import { setUser, readConfig } from "./config.js";
 import {CommandsRegistry,registerCommand,runCommand,handlerLogin} from "./commands.js";
+
 function main() {
+  const registry: CommandsRegistry = [];
+
+  registerCommand(registry, "login", handlerLogin);
+
+  const args = process.argv.slice(2); // the 2 elements for node and the script name 
+
+  if(args.length < 1){
+    console.error("No command provided");
+    process.exit(1);
+  }
+  const [cmdName, ...cmdArgs] = args; // array destructuring)
+
+  try {
+    runCommand(registry,cmdName,...cmdArgs);
+  } catch(error){
+      console.error((error as Error).message);
+    process.exit(1);
+  }
+
   //setUser("Aya");
   //const cfg = readConfig();
   //console.log(cfg);
